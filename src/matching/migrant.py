ce9d16c0eb2a7c9af7ce7f95c05e861e18d37f3a
Tosin Kuye <tkuye77@gmail.com>
2021-10-09 15:49:42 -0600

    Migrant and Mentor changes

--- src/matching/migrant.py
+++ src/matching/migrant.py
@@ -2,6 +2,7 @@
 Module for migrants (immigrants and refugees)
 """
 from matching.user import User
+from matching.mentor import Mentor
 
 class Migrant(User):
 	"""
@@ -11,12 +12,11 @@ class Migrant(User):
 	def __init__(self):
 		super(Migrant, self).__init__()
 		self.__status = ""
+		self.match: Mentor = None
 		
-	
 	def set_status(self, status):
 		self.__status = status
 	
 	def get_status(self):
 		return self.__status
-	
 	
