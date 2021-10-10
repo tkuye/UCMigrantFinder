eb462a1d0a1bb1ca120ded0ec45327453394214e
Tosin Kuye <tkuye77@gmail.com>
2021-10-10 04:29:17 -0600

    chat api

--- src/matching/src/base/migrant.py
+++ src/matching/src/base/migrant.py
@@ -13,7 +13,20 @@ class Migrant(User):
 		super(Migrant, self).__init__()
 		self.__status = ""
 		self.match: str = ""
-		
+		self.__countries = []
+	
+
+	def set_countries(self, countries):
+		"""
+		Setter for our countries
+		"""
+		self.__countries = countries.copy()
+	
+	def get_countries(self):
+		"""
+		Gets our countries as a list
+		"""
+		return self.__countries
 	def set_status(self, status:str):
 		"""
 		Sets the status for a migrant as refugee or immigrant
@@ -21,7 +34,7 @@ class Migrant(User):
 			status (str): refugee or immigrant
 		"""
 		self.__status = status
-	
+
 	def get_status(self):
 		"""
 		Gets the status for a given migrant
@@ -29,13 +42,13 @@ class Migrant(User):
 			str: The migrant status
 		"""
 		return self.__status
-	
+
 
 	def to_dict(self):
 		"""Converts migrant into a dictionary
 		"""
 		migrant = {
-			"country": self.get_country(),
+			"countries": self.get_countries(),
 			"name": self.get_name(),
 			"description": self.get_description(),
 			"languages": self.get_languages(),
@@ -44,9 +57,11 @@ class Migrant(User):
 			"interests": self.get_interests(),
 			"status": self.get_status(),
 			"match": self.get_match(),
+			"room": self.get_room(),
 		}
 
 		return migrant
+
 
 	@staticmethod
 	def dict_to_migrant(migrant_dict: dict):
@@ -57,10 +72,11 @@ class Migrant(User):
 			migrant_dict (dict): the dictionary to convert
 		"""
 		migrant = Migrant()
-		migrant.set_country(migrant_dict.get("country"))
+		migrant.set_countries(migrant_dict.get("countries"))
 		migrant.set_name(migrant_dict.get("name"))
 		migrant.set_demographics(migrant_dict.get("demographics"))
 		migrant.set_languages(migrant_dict.get("languages"))
 		migrant.set_location(migrant_dict.get("location"))
 		migrant.set_interests(migrant_dict.get("interests"))
 		migrant.set_match(migrant_dict.get("match"))
+		migrant.set_room(migrant_dict.get("room"))
